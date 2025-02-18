a50a24b1411b037d6fa221e2011fb20b87d1ac07
413f10r <ale-fioretti@live.it>
2025-02-18 16:25:27 +0100

    start

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+const Header = ()=> {
+   return (
+    <header>
+         <nav>
+                <ul>
+                    <li><a className="text-primary" href="#">Home</a></li>
+                    <li><a href="#" className="active">Prodotti</a></li>
+                    <li><a href="#">Chi siamo</a></li>
+                    <li><a href="#">Contatti</a></li>
+                </ul>
+            </nav>
+    </header>
+)
+
+}
+
+export default Header
