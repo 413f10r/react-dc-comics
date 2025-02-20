a151dd3b056e764c6795d4074fc8395acc38a8f0
413f10r <ale-fioretti@live.it>
2025-02-20 03:34:01 +0100

    add: Main e Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,69 +18,71 @@ const Footer = () => {
     return (
         <>
             <footer >
-                <div className="footerLogo externalLink">
+                <div className="imgFooter">
+                    <div className="logoFooter externalLink">
 
-                    <div>
-                        <ul>
-                            <li><a className="titleFooter" href="#">DC COMICS</a></li>
-                            <li><a href="#">Characters</a></li>
-                            <li><a href="#">Comics</a></li>
-                            <li><a href="#">Movies</a></li>
-                            <li><a href="#">Tv</a></li>
-                            <li><a href="#">Games</a></li>
-                            <li><a href="#">Videos</a></li>
-                            <li><a href="#">News</a></li>
-                        </ul>
+                        <div>
+                            <ul>
+                                <li><a className="titleFooter" href="#">DC COMICS</a></li>
+                                <li><a href="#">Characters</a></li>
+                                <li><a href="#">Comics</a></li>
+                                <li><a href="#">Movies</a></li>
+                                <li><a href="#">Tv</a></li>
+                                <li><a href="#">Games</a></li>
+                                <li><a href="#">Videos</a></li>
+                                <li><a href="#">News</a></li>
+                            </ul>
 
-                        <ul>
-                            <li><a className="titleFooter" href="#">SHOP</a></li>
-                            <li><a href="#">Shop DC</a></li>
-                            <li><a href="#">Shop DC Collectibles</a></li>
-                        </ul>
-                    </div>
+                            <ul>
+                                <li><a className="titleFooter" href="#">SHOP</a></li>
+                                <li><a href="#">Shop DC</a></li>
+                                <li><a href="#">Shop DC Collectibles</a></li>
+                            </ul>
+                        </div>
 
-                    <div>
-                        <ul>
-                            <li><a className="titleFooter" href="#">DC</a></li>
-                            <li><a href="#">Terms Of Use</a></li>
-                            <li><a href="#">Privacy policy(new)</a></li>
-                            <li><a href="#">Ad Choices</a></li>
-                            <li><a href="#">Advertising</a></li>
-                            <li><a href="#">Jobs</a></li>
-                            <li><a href="#">Subscriptions</a></li>
-                            <li><a href="#">Talent Workshops</a></li>
-                            <li><a href="#">CPSC Certificate</a></li>
-                            <li><a href="#">Ratings</a></li>
-                            <li><a href="#">Shop Help</a></li>
-                            <li><a href="#">Contact Us</a></li>
-                        </ul>
-                    </div>
+                        <div>
+                            <ul>
+                                <li><a className="titleFooter" href="#">DC</a></li>
+                                <li><a href="#">Terms Of Use</a></li>
+                                <li><a href="#">Privacy policy(new)</a></li>
+                                <li><a href="#">Ad Choices</a></li>
+                                <li><a href="#">Advertising</a></li>
+                                <li><a href="#">Jobs</a></li>
+                                <li><a href="#">Subscriptions</a></li>
+                                <li><a href="#">Talent Workshops</a></li>
+                                <li><a href="#">CPSC Certificate</a></li>
+                                <li><a href="#">Ratings</a></li>
+                                <li><a href="#">Shop Help</a></li>
+                                <li><a href="#">Contact Us</a></li>
+                            </ul>
+                        </div>
 
-                    <div>
-                        <ul>
-                            <li><a className="titleFooter" href="#">SITES</a></li>
-                            <li><a href="#">DC</a></li>
-                            <li><a href="#">MAD Magazine</a></li>
-                            <li><a href="#">DC Kids</a></li>
-                            <li><a href="#">DC Universe</a></li>
-                            <li><a href="#">DC Power Visa</a></li>
-                        </ul>
+                        <div>
+                            <ul>
+                                <li><a className="titleFooter" href="#">SITES</a></li>
+                                <li><a href="#">DC</a></li>
+                                <li><a href="#">MAD Magazine</a></li>
+                                <li><a href="#">DC Kids</a></li>
+                                <li><a href="#">DC Universe</a></li>
+                                <li><a href="#">DC Power Visa</a></li>
+                            </ul>
+                        </div>
+
                     </div>
 
                 </div>
-
-
             </footer>
 
-            <div className="bgFooter">
+            <div className="backgroundFooter">
 
-                <div className="container contacts">
+                <div className="followUs">
 
-                    <div className="btn">
+                    <div className="button">
                         <a className="titleFooter" href="#">SIGN UP NOW</a>
                     </div>
-                    <ul>
-                        <li><a className="titleFooter blue" href="#">FOLLOW US</a></li>
+                    <ul
+                        className="linkSocial">
+                        <li><a className="titleFooter " href="#">FOLLOW US</a></li>
                         <li><a href="#"><img src="../assets/img/footer-facebook.png" alt="" /></a></li>
                         <li><a href="#"><img src={facebook} alt="facebook" /></a></li>
                         <li><a href="#"><img src={periscope} alt="periscope" /></a></li>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -12,7 +12,16 @@ const Main = () => {
                 <p> __Content Goes Here__ </p>
             </section>
 
-            <section className="BluContainer"></section>
+            <section className="BluContainer">
+                <ul className="icons">
+                    <li><a href="#"> <img src={digitalComics} alt="digitalComics" /></a><p>digital comics</p></li>
+                    <li><a href="#"> <img src={dcMerchandise} alt="dcMerchandise" /></a><p>dc merchandise</p></li>
+                    <li><a href="#"> <img src={shopLocator} alt="shopLocator" /></a><p>subscriptions</p></li>
+                    <li><a href="#"> <img src={subscriptions} alt="subscriptions" /></a><p>comic shop locator</p></li>
+                    <li><a href="#"> <img src={dcPowerVisa} alt="dcPowerVisa" /></a><p>dc power visa</p></li>
+                </ul>
+
+            </section>
 
         </main>
     )
